2c2013385df321fc3e17da591903862bc6dec322
chenop <selavi99>
2014-07-18 15:57:14 +0300

    adds client directory

---
+++ client/lib/bootstrap-rtl/Gruntfile.js
@@ -0,0 +1,55 @@
+module.exports = function(grunt) {
+  "use strict";
+
+  // Project configuration
+  grunt.initConfig({
+    // Read package.json Metadata.
+    pkg: grunt.file.readJSON('package.json'),
+    banner: '/*******************************************************************************\n' +
+            ' *              <%= pkg.name %> (Version <%= pkg.version %>)\n' +
+            ' *      Author: <%= pkg.author %>\n' +
+            ' *  Created on: <%= grunt.template.today("mmmm dd,yyyy") %>\n' +
+            ' *     Project: <%= pkg.name %>\n' +
+            ' *   Copyright: See the file "LICENSE.md" for the full license governing this code.\n' +
+            ' *******************************************************************************/\n',
+    less: {
+      rtl: {
+        options: {
+          strictMath: true,
+          cleancss: false
+        },
+        files: {
+          'dist/css/<%= pkg.name %>.css': 'less/bootstrap-rtl.less'
+        }
+      },
+      minify: {
+        options: {
+          cleancss: true
+        },
+        files: {
+          'dist/css/<%= pkg.name %>.min.css': 'dist/css/<%= pkg.name %>.css',
+        }
+      }
+    },
+
+    usebanner: {
+        options: {
+          position: 'top',
+          banner: '<%= banner %>',
+          linebreak: true
+
+        },
+        files: {
+          src: ['dist/css/<%= pkg.name %>.css', 'dist/css/<%= pkg.name %>.min.css']
+        }        
+    }
+  });
+
+
+// Load uglify plugin
+grunt.loadNpmTasks('grunt-contrib-less');
+grunt.loadNpmTasks('grunt-banner');
+
+// Default Task
+grunt.registerTask('default', ['less', 'usebanner']);
+};
