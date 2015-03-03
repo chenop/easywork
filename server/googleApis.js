f27a6ea4ee1862680c0427c7aa24bb2727f10cff
Chen Oppenhaim <chenop@gmail.com>
2015-03-03 16:32:12 +0200

    - expand googleApis

--- server/googleApis.js
+++ server/googleApis.js
@@ -6,17 +6,34 @@ var google = require('googleapis')
 var CX = '008985495639693800406:puf4b2va1gk';
 var API_KEY = 'AIzaSyB2FUs8REJu3r759qD-xSGpKhLPU6rAoY4';
 
-exports.fetchFirstImage = function(keyword, callback) {
-    return customsearch.cse.list({ cx: CX, q: keyword, searchType: 'image', auth: API_KEY }, function(err, resp) {
+function fetchImages(keyword, callback) {
+    return customsearch.cse.list({cx: CX, q: keyword, searchType: 'image', auth: API_KEY}, function (err, resp) {
         if (err) {
             return callback(err);
         }
-        // Got the response from custom search
-        console.log('Result: ' + resp.searchInformation.formattedTotalResults);
-        if (resp.items && resp.items.length > 0) {
-            var firstItem = resp.items[0].link;
-            return callback(null, firstItem);
-        }
-    });
+        var images = [];
 
+        // Got the response from custom search
+        if (resp.items && resp.items.length > 0) {
+
+            resp.items.forEach(function(item) {
+                images.push(item.link);
+            });
+        }
+        return callback(null, images);
+    });
+}
+
+exports.fetchFirstImage = function(keyword, callback) {
+    return fetchImages(keyword, function(err, images) {
+        if (err) {
+            return callback(err);
+        }
+
+        return callback(images[0]);
+    });
+}
+
+exports.fetchImages = function(keyword, callback) {
+    return fetchImages(keyword, callback);
 }
