26b4195ff68ad576deeb9b95abf70bf787425e28
Chen <chenop@gmail.com>
2015-04-08 00:59:28 +0300

    - Convert to hebrew names - Adding the connection between company and jobs

--- server/api/jobs.js
+++ server/api/jobs.js
@@ -9,6 +9,43 @@ var fs = require('fs')
     , Job = require('../model/job')
     , Company = require('../model/company')
 
+Array.prototype.equals = function (array, strict) {
+    if (!array)
+        return false;
+
+    if (arguments.length == 1)
+        strict = true;
+
+    if (this.length != array.length)
+        return false;
+
+    for (var i = 0; i < this.length; i++) {
+        if (this[i] instanceof Array && array[i] instanceof Array) {
+            if (!this[i].equals(array[i], strict))
+                return false;
+        }
+        else if (strict && this[i] != array[i]) {
+            return false;
+        }
+        else if (!strict) {
+            return this.sort().equals(array.sort(), true);
+        }
+    }
+    return true;
+}
+
+Array.prototype.merge = function(/* variable number of arrays */){
+    for(var i = 0; i < arguments.length; i++){
+        var array = arguments[i];
+        for(var j = 0; j < array.length; j++){
+            if(this.indexOf(array[j]) === -1) {
+                this.push(array[j]);
+            }
+        }
+    }
+    return this;
+};
+
 exports.createJob = function (req, res) {
     return Company.findById(req.body.company, function(err, company) {
 
@@ -25,33 +62,61 @@ exports.createJob = function (req, res) {
 
         // Adding job to company
         company.jobs.push(newJob);
-        company.save(function (err) {
+        return company.save(function (err) {
             if (err) {
                 console.log("Error saving job in company " + company.name);
             }
-        })
 
-        newJob.save(function (err) {
-            if (!err) {
-                console.log("job " + newJob.name + " create in server")
-                return res.send(newJob);
-            } else {
-                console.log(err);
-            }
-        });
-        return res.send(newJob);
+            return newJob.save(function (err) {
+                if (!err) {
+                    console.log("job " + newJob.name + " create in server")
+                    return res.send(newJob);
+                } else {
+                    console.log(err);
+                    return res.json(401, err);
+                }
+            });
+        })
     })
 }
 
+function updateCompanyTechnologies(company) {
+    Company.findById(company, function (err, company) {
+        if (company === undefined || company == null)
+            return;
+
+        if (err)
+            return console.log(err);
+
+        Job.find({company: company}, function (err, companyJobs) {
+
+            var mergedTechnologies = [];
+            for (var i = 0; i < companyJobs.length; i++) {
+                mergedTechnologies.merge(companyJobs[i].technologies);
+            }
+
+            company.technologies = mergedTechnologies;
+
+            company.save();
+
+        });
+    });
+}
 exports.updateJob = function (req, res) {
     return Job.findById(req.params.id, function (err, job) {
         if (job === undefined || job == null)
             return;
-        job.name = req.body.name
-            , job.code = req.body.code
-            , job.city = req.body.city
-            , job.technologies = req.body.technologies
-            , job.description = req.body.description
+
+        job.name = req.body.name;
+        job.code = req.body.code;
+        job.city = req.body.city;
+        job.description = req.body.description;
+
+        if (!job.technologies.equals(req.body.technologies)) {
+            job.technologies = req.body.technologies;
+
+            updateCompanyTechnologies(job.company);
+        }
 
         return job.save(function (err) {
             if (!err) {
@@ -88,24 +153,30 @@ exports.deleteJob = function (req, res) {
     var jobId = req.params.id;
     return Job.findById(jobId, function (err, job) {
         // Remove job from company
+        if (!job) {
+            return res.json(501, "job not found");
+        }
+
         var company = job.company;
         if (company !== null || company !== undefined) {
-            Company.findById(company, function (err, company) {
+            return Company.findById(company, function (err, company) {
                 if (company === null || company === undefined)
                     return;
                 company.jobs.remove(jobId);
-                company.save();
+                return company.save(function(err, savedCompany) {
+
+                    // Remove job from collection jobs
+                    return job.remove(function (err) {
+                        if (!err) {
+                            return res.send(job);
+                        } else {
+                            console.log(err);
+                            return res.json(401, err);
+                        }
+                    });
+                });
             })
         }
-
-        // Remove job from collection jobs
-        return job.remove(function (err) {
-            if (!err) {
-                return res.send(job);
-            } else {
-                console.log(err);
-            }
-        });
     });
 }
 
@@ -120,14 +191,23 @@ exports.getJob = function (req, res) {
 }
 
 exports.getJobs = function (req, res) {
-    return Job.find({ 'company': req.params.id}, function (err, jobs) {
+    return getJobsByCompanyId(req.params.id);
+};
+
+var getJobsByCompanyId = function(companyId, callback) {
+    return Job.find({'company': companyId}, function (err, jobs) {
         if (!err) {
-            return res.send(jobs);
+            if (callback) {
+                callback(jobs)
+            }
+            else {
+                return jobs;
+            }
         } else {
             return console.log(err);
         }
     });
-};
+}
 
 exports.getAllJobs = function(req, res) {
     Job.find().select('company name description city technologies').populate('company').lean()
