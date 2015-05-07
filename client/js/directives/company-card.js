b21cd5fdea560e3cfe025481b603fbeadc0eb1ae
Chen Oppenhaim <chenop@gmail.com>
2015-05-07 08:48:57 +0300

    - Add Job modal capability

--- client/js/directives/company-card.js
+++ client/js/directives/company-card.js
@@ -18,8 +18,35 @@ angular.module('easywork')
         return {
             restrict: 'E',
             scope: {
-                requiredSkill: '='
+                requiredSkill: '=',
+                company: '='
             },
-            template: '<span class="skill-tag"><i class="glyphicon glyphicon-tag skill-icon"></i>{{::requiredSkill}}</span>'
+            controller: function($scope, $modal) {
+                $scope.showJobDetails = function (company, event) {
+                    // Do not propagate the event to the table
+                    if(event){
+                        event.stopPropagation();
+                        event.preventDefault();
+                    }
+                    var modalInstance = $modal.open({
+                        templateUrl: '/views/companies/company-details.html',
+                        controller: 'CompanyDetailsCtrl',
+                        windowClass: 'company-details-dialog',
+                        resolve: {
+                            company: function() {
+                                return company;
+                            }
+                        }
+
+                    });
+
+                    modalInstance.result.then(function (username) {
+                        if (username != undefined)
+                            console.log('User: ' + username + ' has logged in');
+                    });
+                }
+
+            },
+            template: '<span class="skill-tag"><a ng-click="showJobDetails(company, $event)"><i class="glyphicon glyphicon-tag skill-icon"></i>{{::requiredSkill}}</a></span>'
         }
     })
