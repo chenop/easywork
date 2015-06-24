fc764d0a16749622bdc1cd9196b295163bfff6da
Chen.Oppenhaim <chenop@gmail.com>
2015-06-24 05:59:48 +0300

    - Styling modal

--- client/js/controllers/job-details-controller.js
+++ client/js/controllers/job-details-controller.js
@@ -7,7 +7,6 @@ angular.module('easywork')
 
         dataManager.getJobsBySkill($scope.requiredSkill,$scope.company._id).
             then(function (data) {
-                console.log($scope.requiredSkill);
                 if (data && data.data && data.data.length > 0 && data.data[0].name) {
                     $scope.job = data.data[0];
                 }
