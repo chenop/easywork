a375646dc82aaee3f2e209a99fc9f3874e82041c
Chen.Oppenhaim <chenop@gmail.com>
2015-10-02 22:59:36 +0300

    Merge branch 'master' of C:\Users\chen.oppenhaim\My Projects\easywork-AngularJS-NodeJS with conflicts.

--- Gruntfile.js
+++ Gruntfile.js
@@ -79,6 +79,7 @@ module.exports = function (grunt) {
 	grunt.loadNpmTasks('grunt-contrib-watch');
 	grunt.loadNpmTasks('grunt-express-server');
 	grunt.loadNpmTasks('grunt-open');
+    grunt.loadNpmTasks('grunt-contrib-uglify');
 	grunt.loadNpmTasks('grunt-ng-annotate');
 
 	grunt.registerTask('default', ['express:dev', 'open', 'watch' ])
