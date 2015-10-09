b4616ead2ab88f7395efdc63f9224d32a576a3df
Chen <chenop@gmail.com>
2015-10-09 11:32:19 +0300

    - Grunt deploying process - grunt-template making  sounds

--- Gruntfile.js
+++ Gruntfile.js
@@ -5,13 +5,89 @@ module.exports = function (grunt) {
 	var cssFiles = [
 			'https://maxcdn.bootstrapcdn.com/bootstrap/3.1.1/css/bootstrap.min.css'
 			, './lib/bootstrap-rtl/dist/css/bootstrap-rtl.css'
-			, './lib/jasny-bootstrap/dist/css/jasny-bootstrap.min.css'
 			, './lib/select2/select2.css'
 			, './lib/select2/select2.css'
 			, './lib/angularjs-toaster/toaster.min.css'
 			, '//netdna.bootstrapcdn.com/font-awesome/4.0.0/css/font-awesome.css'
 			, './css/style.css'
 		];
+
+	var vendorJsFiles = [
+
+		'https://code.jquery.com/jquery-2.1.1.min.js'
+		, './lib/bootstrap-rtl/dist/js/html5shiv.js'
+		, './lib/bootstrap-rtl/dist/js/respond.min.js'
+		, './lib/ng-file-upload/angular-file-upload-shim.min.js'
+
+		// Angular
+		, './lib/angular/angular.js'
+		, './lib/angular-cookies/angular-cookies.js'
+		, './lib/angular-animate/angular-animate.js'
+		, './lib/angular-sanitize/angular-sanitize.js'
+		, './lib/angular-messages/angular-messages.js'
+		, './lib/angular-bootstrap/ui-bootstrap-tpls.js'
+		, './lib/angular-ui-router/release/angular-ui-router.js'
+
+		// Ui-Select2
+		, './lib/select2/select2.js'
+		, './lib/angular-ui-select2/src/select2.js'
+
+		// Toaster
+		, './lib/angularjs-toaster/toaster.js'
+
+		// Parsing CVs
+		, './lib/jszip/dist/jszip.js'
+		, './lib/docx/docx.js'
+
+		// File Upload - - danial.farid
+		, './lib/ng-file-upload/angular-file-upload.js'
+
+		, './lib/bootstrap-rtl/dist/js/holder.js'
+
+		// localForage
+		, './lib/localforage/dist/localforage.js'
+		, './lib/angular-localforage/dist/angular-localForage.js'
+	];
+
+	var appJsFiles = [
+		'./js/app.js'
+
+		,'./js/routingConfig.js'
+		,'./js/controllers/header-controller.js'
+		,'./js/controllers/job-board-controller.js'
+		,'./js/controllers/company-board-controller.js'
+		,'./js/controllers/company-controller.js'
+		,'./js/controllers/home-controller.js'
+		,'./js/controllers/dashboard-controller.js'
+		,'./js/controllers/dashboard-list-controller.js'
+		,'./js/controllers/user-controller.js'
+		,'./js/controllers/job-controller.js'
+		,'./js/controllers/job-full-controller.js'
+		,'./js/controllers/company-details-controller.js'
+		,'./js/controllers/job-details-controller.js'
+		,'./js/controllers/login-register-controller.js'
+		,'./js/controllers/empty-controller.js'
+		,'./js/controllers/logo-gallery-controller.js'
+		,'./js/controllers/send-cv-dialog-controller.js'
+
+		,'./js/services/app-manager-service.js'
+		,'./js/controllers/login-controller.js'
+		,'./js/controllers/register-controller.js'
+		,'./js/services/data-manager-service.js'
+		,'./js/services/auth-service.js'
+		,'./js/services/mail-service.js'
+		,'./js/services/cv-parser.js'
+		,'./js/services/common-service.js'
+		,'./js/services/utils-services.js'
+
+		,'./js/models/job.js'
+		,'./js/models/modelTransformer.js'
+
+		,'./js/directives/directives.js'
+		,'./js/directives/company-card.js'
+		,'./js/directives/user-card.js'
+		,'./js/directives/upload-cv-directive.js'
+	]
 
 
 	var generateFilesList = function(files, isConcat, fileType) {
@@ -28,14 +104,19 @@ module.exports = function (grunt) {
 
 			switch (fileType) {
 				case ('css') : {
+					preFix = "\t<link href=";
+					postFix = " rel=\"stylesheet\">\n";
+					break;
+				}
+ 				case ('js') : {
 					preFix = "\t<script type=\"text/javascript\" src=\"";
 					postFix = "\"></script>\n";
+					break;
 				}
 			}
 
 			for(var i = 0, len = files.length; i < len; i++) {
 				result += preFix + files[i] + postFix;
-				console.log(preFix + files[i] + postFix)
 			}
 		}
 
@@ -70,7 +151,9 @@ module.exports = function (grunt) {
 			'process-html-template': {
 				'options': {
 					'data': {
-						'cssFiles': generateFilesList(cssFiles, false, "css")
+						cssFiles: generateFilesList(cssFiles, false, "css"),
+						vendorJsFiles: generateFilesList(vendorJsFiles, false, "js"),
+						appJsFiles: generateFilesList(appJsFiles, false, "js")
 					}
 				},
 				'files': {
