bcb16cba56420ddeb0db22a0f33a88923e2e94e3
Chen Oppenhaim <chenop@gmail.com>
2015-10-11 19:57:38 +0300

    - Enable register as a jobProvider

--- Gruntfile.js
+++ Gruntfile.js
@@ -2,95 +2,100 @@ var path = require('path');
 
 module.exports = function (grunt) {
 
+
 	var cssFiles = [
 			'https://maxcdn.bootstrapcdn.com/bootstrap/3.1.1/css/bootstrap.min.css'
-			, './lib/bootstrap-rtl/dist/css/bootstrap-rtl.css'
-			, './lib/select2/select2.css'
-			, './lib/select2/select2.css'
-			, './lib/angularjs-toaster/toaster.min.css'
+			, 'client/lib/bootstrap-rtl/dist/css/bootstrap-rtl.css'
+			, 'client/lib/select2/select2.css'
+			, 'client/lib/select2/select2.css'
+			, 'client/lib/angularjs-toaster/toaster.min.css'
 			, '//netdna.bootstrapcdn.com/font-awesome/4.0.0/css/font-awesome.css'
-			, './css/style.css'
+			, 'client/css/style.css'
 		];
 
 	var vendorJsFiles = [
 
 		'https://code.jquery.com/jquery-2.1.1.min.js'
-		, './lib/bootstrap-rtl/dist/js/html5shiv.js'
-		, './lib/bootstrap-rtl/dist/js/respond.min.js'
-		, './lib/ng-file-upload/angular-file-upload-shim.min.js'
+		, 'client/lib/bootstrap-rtl/dist/js/html5shiv.js'
+		, 'client/lib/bootstrap-rtl/dist/js/respond.min.js'
+		, 'client/lib/ng-file-upload/angular-file-upload-shim.min.js'
 
 		// Angular
-		, './lib/angular/angular.js'
-		, './lib/angular-cookies/angular-cookies.js'
-		, './lib/angular-animate/angular-animate.js'
-		, './lib/angular-sanitize/angular-sanitize.js'
-		, './lib/angular-messages/angular-messages.js'
-		, './lib/angular-bootstrap/ui-bootstrap-tpls.js'
-		, './lib/angular-ui-router/release/angular-ui-router.js'
+		, 'client/lib/angular/angular.js'
+		, 'client/lib/angular-cookies/angular-cookies.js'
+		, 'client/lib/angular-animate/angular-animate.js'
+		, 'client/lib/angular-sanitize/angular-sanitize.js'
+		, 'client/lib/angular-messages/angular-messages.js'
+		, 'client/lib/angular-bootstrap/ui-bootstrap-tpls.js'
+		, 'client/lib/angular-ui-router/release/angular-ui-router.js'
 
 		// Ui-Select2
-		, './lib/select2/select2.js'
-		, './lib/angular-ui-select2/src/select2.js'
+		, 'client/lib/select2/select2.js'
+		, 'client/lib/angular-ui-select2/src/select2.js'
 
 		// Toaster
-		, './lib/angularjs-toaster/toaster.js'
+		, 'client/lib/angularjs-toaster/toaster.js'
 
 		// Parsing CVs
-		, './lib/jszip/dist/jszip.js'
-		, './lib/docx/docx.js'
+		, 'client/lib/jszip/dist/jszip.js'
+		, 'client/lib/docx/docx.js'
 
 		// File Upload - - danial.farid
-		, './lib/ng-file-upload/angular-file-upload.js'
+		, 'client/lib/ng-file-upload/angular-file-upload.js'
 
-		, './lib/bootstrap-rtl/dist/js/holder.js'
+		, 'client/lib/bootstrap-rtl/dist/js/holder.js'
 
 		// localForage
-		, './lib/localforage/dist/localforage.js'
-		, './lib/angular-localforage/dist/angular-localForage.js'
+		, 'client/lib/localforage/dist/localforage.js'
+		, 'client/lib/angular-localforage/dist/angular-localForage.js'
 	];
 
 	var appJsFiles = [
-		'./js/app.js'
+		'client/js/app.js'
 
-		,'./js/routingConfig.js'
-		,'./js/controllers/header-controller.js'
-		,'./js/controllers/job-board-controller.js'
-		,'./js/controllers/company-board-controller.js'
-		,'./js/controllers/company-controller.js'
-		,'./js/controllers/home-controller.js'
-		,'./js/controllers/dashboard-controller.js'
-		,'./js/controllers/dashboard-list-controller.js'
-		,'./js/controllers/user-controller.js'
-		,'./js/controllers/job-controller.js'
-		,'./js/controllers/job-full-controller.js'
-		,'./js/controllers/company-details-controller.js'
-		,'./js/controllers/job-details-controller.js'
-		,'./js/controllers/login-register-controller.js'
-		,'./js/controllers/empty-controller.js'
-		,'./js/controllers/logo-gallery-controller.js'
-		,'./js/controllers/send-cv-dialog-controller.js'
+		,'client/js/routingConfig.js'
+		,'client/js/controllers/header-controller.js'
+		,'client/js/controllers/job-board-controller.js'
+		,'client/js/controllers/company-board-controller.js'
+		,'client/js/controllers/company-controller.js'
+		,'client/js/controllers/home-controller.js'
+		,'client/js/controllers/dashboard-controller.js'
+		,'client/js/controllers/dashboard-list-controller.js'
+		,'client/js/controllers/user-controller.js'
+		,'client/js/controllers/job-controller.js'
+		,'client/js/controllers/job-full-controller.js'
+		,'client/js/controllers/company-details-controller.js'
+		,'client/js/controllers/job-details-controller.js'
+		,'client/js/controllers/login-register-controller.js'
+		,'client/js/controllers/empty-controller.js'
+		,'client/js/controllers/logo-gallery-controller.js'
+		,'client/js/controllers/send-cv-dialog-controller.js'
 
-		,'./js/services/app-manager-service.js'
-		,'./js/controllers/login-controller.js'
-		,'./js/controllers/register-controller.js'
-		,'./js/services/data-manager-service.js'
-		,'./js/services/auth-service.js'
-		,'./js/services/mail-service.js'
-		,'./js/services/cv-parser.js'
-		,'./js/services/common-service.js'
-		,'./js/services/utils-services.js'
+		,'client/js/services/app-manager-service.js'
+		,'client/js/controllers/login-controller.js'
+		,'client/js/controllers/register-controller.js'
+		,'client/js/services/data-manager-service.js'
+		,'client/js/services/auth-service.js'
+		,'client/js/services/mail-service.js'
+		,'client/js/services/cv-parser.js'
+		,'client/js/services/common-service.js'
+		,'client/js/services/utils-services.js'
 
-		,'./js/models/job.js'
-		,'./js/models/modelTransformer.js'
+		,'client/js/models/job.js'
+		,'client/js/models/modelTransformer.js'
 
-		,'./js/directives/directives.js'
-		,'./js/directives/company-card.js'
-		,'./js/directives/user-card.js'
-		,'./js/directives/upload-cv-directive.js'
+		,'client/js/directives/directives.js'
+		,'client/js/directives/company-card.js'
+		,'client/js/directives/user-card.js'
+		,'client/js/directives/upload-cv-directive.js'
 	]
 
+	var isDevMode = function() {
+		console.log('isDevMode: ' + process.env.NODE_ENV);
+		return process.env.NODE_ENV === 'development';
+	}
 
-	var generateFilesList = function(files, isConcat, fileType) {
+	var generateFilesList = function(files, minifiedFileName, fileType) {
 		//var files = grunt.config('files'),
 		//	isConcat = grunt.config('isConcat');
 
@@ -101,8 +106,8 @@ module.exports = function (grunt) {
 
 		switch (fileType) {
 			case ('css') : {
-				preFix = "\t<link href=";
-				postFix = " rel=\"stylesheet\">\n";
+				preFix = "\t<link href=\"";
+				postFix = "\" rel=\"stylesheet\">\n";
 				break;
 			}
 			case ('js') : {
@@ -112,22 +117,49 @@ module.exports = function (grunt) {
 			}
 		}
 
-		if(isConcat) {
-			result = preFix + grunt.config('concat.dist.dest') + postFix;
-		} else {
+		if(isDevMode()) {
 			for(var i = 0, len = files.length; i < len; i++) {
-				result += preFix + files[i] + postFix;
+				var filePath = removeClientDirFromPath(files[i]);
+				result += preFix + filePath + postFix;
 			}
+		}
+		else { // Production
+			var filePath = removeClientDirFromPath(minifiedFileName);
+			result = preFix + filePath + postFix;
+			console.log('res: ' + result)
 		}
 
 		return result;
 	}
 
-	grunt.initConfig({
+	var removeClientDirFromPath = function(filePath) {
+		return filePath.replace(/client/, ".");
+	}
 
-		cssFiles: generateFilesList(cssFiles, false, "css"),
-		vendorJsFiles: generateFilesList(vendorJsFiles, false, "js"),
-		appJsFiles: generateFilesList(appJsFiles, false, "js"),
+	var minifiedCssFile = 'client/dist/app.css',
+		minifiedVendorsFile = 'client/dist/vendors.js',
+		minifiedAppFile = 'client/dist/app.js';
+
+		grunt.initConfig({
+
+
+		minifiedCssFile: minifiedCssFile,
+		minifiedVendorsFile: minifiedVendorsFile,
+		minifiedAppFile: minifiedAppFile,
+		cssFiles: generateFilesList(cssFiles, minifiedCssFile, "css"),
+		vendorJsFiles: generateFilesList(vendorJsFiles, minifiedVendorsFile, "js"),
+		appJsFiles: generateFilesList(appJsFiles, minifiedAppFile, "js"),
+		env : {
+			options : {
+				//Shared Options Hash
+			},
+			dev : {
+				NODE_ENV : 'development'
+			},
+			prod : {
+				NODE_ENV : 'production'
+			}
+		},
 		express: {
 			options: {
 				port : process.env.PORT || 3000
@@ -138,26 +170,15 @@ module.exports = function (grunt) {
 					script: 'server.js'
 //					debug : true //enable debugging
 				}
-			},
-			prod: {
-				options: {
-					script: 'dist/app.js',
-					node_env: 'production'
-				}
-			},
-			test: {
-				options: {
-					script: 'path/to/test/server.js'
-				}
 			}
 		},
 		template: {
 			'process-html-template': {
 				'options': {
 					'data': {
-						cssFiles: cssFiles,
-						vendorJsFiles: vendorJsFiles,
-						appJsFiles: appJsFiles
+						cssFiles: '<%= cssFiles %>',
+						vendorJsFiles:  '<%= vendorJsFiles %>',
+						appJsFiles: '<%= appJsFiles %>',
 					}
 				},
 				'files': {
@@ -203,34 +224,50 @@ module.exports = function (grunt) {
             },
             dist: {
                 files: {
-                    'dist/app.js' : cssFiles.concat(vendorJsFiles).concat(appJsFiles),
+                    'client/dist/vendors.js' : vendorJsFiles,
+                    'client/dist/app.js' : appJsFiles
                 },
             },
         },
-		uglify: {
-			dist: {
+		cssmin: {
+			options: {
+				shorthandCompacting: false,
+				roundingPrecision: -1
+			},
+			target: {
 				files: {
-					'dist/app.js': [ 'dist/app.js' ]
+					'client/dist/app.css' : cssFiles
+				}
+			}
+		},
+		uglify: {
+			prod: {
+				files: {
+					'client/dist/vendors.js' : '<%= minifiedVendorsFile %>',
+					'client/dist/app.js' : '<%= minifiedAppFile %>'
 				}
 			}
 		},
 		clean: {
 			temp: {
-				src: [ 'tmp' , 'client/index.html']
+				src: [ 'dist' , 'client/index.html']
 			}
 		}
 	});
 
 	grunt.loadNpmTasks('grunt-template');
+	grunt.loadNpmTasks('grunt-env');
 	grunt.loadNpmTasks('grunt-contrib-watch');
 	grunt.loadNpmTasks('grunt-contrib-clean');
 	grunt.loadNpmTasks('grunt-express-server');
 	grunt.loadNpmTasks('grunt-open');
 	grunt.loadNpmTasks('grunt-ng-annotate');
 	grunt.loadNpmTasks('grunt-contrib-uglify');
+	grunt.loadNpmTasks('grunt-contrib-cssmin');
 
 	grunt.registerTask('default', ['express:dev', 'open', 'watch' ]);
-	grunt.registerTask('minify', ['ngAnnotate:dist', 'clean']);
+	grunt.registerTask('dev', ['template', 'default']);
+	grunt.registerTask('prod', ['clean', 'template', 'cssmin', 'ngAnnotate', 'uglify:prod', 'express:dev', 'open']);
 	grunt.registerTask('runTemplate', ['clean', 'template']);
 }
 ;
