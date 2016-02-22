dd9af534fd02a67fe39b18494ca3aec646ce344e
Chen.Oppenhaim <chenop@gmail.com>
2016-02-22 15:07:44 +0200

    - use docParserUrl

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -154,7 +154,7 @@ function analyzeCv(fileName, fileData) {
 
     var options = {
         method: 'POST',
-        uri: 'http://localhost:8080/webapi/files/upload',
+        uri: config.docParserUrl,
         formData: formData,
         headers: {
             'content-type': 'multipart/form-data' // Set automatically
