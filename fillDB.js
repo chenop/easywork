dbfd713e48ff0ef42f09c0142772a735489b3707
Chen <chenop@gmail.com>
2016-04-10 11:43:22 +0300

    - Support connecting to Doc Parser

--- fillDB.js
+++ fillDB.js
@@ -1,8 +1,7 @@
-
-var mongoose = require('mongoose')
-, companyService = require('./server/services/companyService')
-    , Company = require('./server/models/company')
-    , config = require('./server/config')
+var mongoose         = require('mongoose')
+    , companyService = require('./server/services/companyService')
+    , Company        = require('./server/models/company')
+    , config         = require('./server/config')
     ;
 var start = Date.now();
 var log = function (message) {
@@ -15,7 +14,7 @@ var dbUrl = config.dbUrl;
 
 console.log("DB URL: " + dbUrl);
 mongoose.connect(dbUrl); // comment
-mongoose.connection.on('error', function(err, req, res, next)  {
+mongoose.connection.on('error', function (err, req, res, next) {
     log("Cant connect to MongoDB - please verify that it was started.");
 });
 mongoose.connection.once('open', function callback() {
@@ -73,10 +72,10 @@ function searchCity(address) {
 
 function createCompany(company) {
     var newAddresses = reformatAddresses(company);
-    var newCompany = new Company ({
-        name : company.name
-        , site : company.site
-        , description : company.description
+    var newCompany = new Company({
+        name: company.name
+        , site: company.site
+        , description: company.description
         , street: company.street
         , addresses: newAddresses
         , city: company.city
@@ -144,27 +143,44 @@ function updateCompany(company) {
 // TODO Chen - trying to move the city (inside the street) to the city field
 // 1. find a specific company with multi locations
 // 2. cut and paste the city
-// 3. repeatfor all
-Company.find({}, function (err, companies) {
+// 3. repeat for all
+var extractCity = function (street) {
+    for (var i = 0; i < cities.length; i++) {
+        var city = cities[i];
 
-    for (var i = 0; i < companies.length; i++) {
-        var company = companies[i];
-
-        // Setting locations
-        for (var l = 0; l < company.locations.length; l++) {
-            var location = company.locations[l];
-
-            if (!location || !(location.street))
-                continue;
-
-            var street = location.street;
-            console.log(street);
-
-        }
-
-        if (i == 3)
-            return;
-
-        //companyService.updateCompany(company);
+        //if (street.toLowerCase().indexOf(city.toLowerCase()) > -1)
+        if (street.indexOf(city) > -1)
+            return city;
     }
-});
+
+    return null;
+};
+
+companyService.getCompanies()
+    .then(function (companies) {
+        for (var i = 0; i < companies.length; i++) {
+            var company = companies[i];
+
+            // Setting locations
+            for (var l = 0; l < company.locations.length; l++) {
+                var location = company.locations[l];
+
+                if (!location || !(location.street))
+                    continue;
+
+                var street = location.street;
+                console.log(i + ": " + street);
+
+                var cityFound = extractCity(street);
+                if (cityFound) {
+                    console.log("found! " + cityFound);
+                    company.city = cityFound;
+                }
+            }
+
+            if (i == 30)
+                return;
+
+            //companyService.updateCompany(company);
+        }
+    });
