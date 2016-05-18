21995c6753a723d2ca5c7f450bfcf6341608331c
chenop <selavi99>
2016-05-18 21:40:33 +0300

    - Cv implemented Controler & Service

--- Gruntfile.js
+++ Gruntfile.js
@@ -264,6 +264,6 @@ module.exports = function (grunt) {
     grunt.registerTask('dev', ['template', 'default']);
     grunt.registerTask('prod', ['clean', 'template', 'cssmin', 'ngAnnotate', 'uglify:prod']);
     // grunt.registerTask('runTemplate', ['clean', 'template']);
-}; //
+};
 
 
