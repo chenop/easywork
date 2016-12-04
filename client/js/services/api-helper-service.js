d0e892ff19b898db195297dcf6ef6a2236795c9e
Chen <chenop@gmail.com>
2016-12-04 17:01:00 +0200

    - Continue with security protection - Fix tests

---
+++ client/js/services/api-helper-service.js
@@ -0,0 +1,37 @@
+/**
+ * Created by Chen on 27/11/2016.
+ */
+
+angular.module('easywork')
+	.factory('apiHelper', function ($http) {
+		function prefix(isPublic) {
+			return isPublic ? "/public/" : "/api/";
+		}
+
+		function get(isPublic, url, params) {
+			if (!params)
+				params = {};
+
+			return $http.get(prefix(isPublic) + url, params);
+		}
+
+		function post(isPublic, url, data) {
+			return $http.post(prefix(isPublic) + url, data);
+		}
+
+		function put(isPublic, url, data) {
+			return $http.put(prefix(isPublic) + url, data);
+		}
+
+		function remove(isPublic, url) {
+			return $http.delete(prefix(isPublic) + url);
+		}
+
+		return {
+			get: get
+			, post: post
+			, put: put
+			, remote: remove
+		}
+	});
+
