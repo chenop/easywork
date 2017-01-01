a43912918718e4dbd6726ac10a131458aa91a089
Chen.Oppenhaim <chenop@gmail.com>
2017-01-01 11:21:39 +0200

    - Slowly regressing from TypeScript...

--- server/controllers/feedbackController.js
+++ server/controllers/feedbackController.js
@@ -1,20 +1,25 @@
 /**
  * Created by Chen on 19/10/2016.
  */
-var mailService = require('../mail');
+
+var mailService     = require('../mail');
+
 /***********
  * Public
  ***********/
 module.exports = {
     sendFeedback: sendFeedback
-};
+}
+
 /***********
  * Private
  ***********/
 function sendFeedback(req, res) {
     var data = req.body.data;
+
     if (!data.content)
         return; // todo write error to log
+
     mailService.sendFeedbackMail(data);
     return res.send("success");
 }
