10678f890454ce732837d9ba56235d2b987b93b9
Chen <chenop@gmail.com>
2017-03-14 21:27:14 +0200

    Rolling back...

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 //require('newrelic');
-console.log("process.env.NODE_ENV: " + process.env.NODE_ENV);
 if (!process.env.NODE_ENV) process.env.NODE_ENV = 'development'
+console.log("process.env.NODE_ENV: " + process.env.NODE_ENV);
+
 
 // To enable sending mail
 // https://stackoverflow.com/questions/20433287/node-js-request-cert-has-expired/20497028#20497028
