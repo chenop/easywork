9e367d1a2f00d4d2d66252e38c9525ca243c2c4b
Chen Oppenhaim <chen.oppenhaim@toluna.com>
2020-08-17 09:49:02 +0300

    Updating Atlas URI

--- server/config.js
+++ server/config.js
@@ -4,7 +4,7 @@
 
 var TEST_DB_URL = "mongodb://chenop:selavi99@ds039185.mongolab.com:39185/heroku_hjgps9xv";
 //var PRODUCTION_DB = "mongodb://chenop:selavi99@ds061188.mongolab.com:61188/heroku_app27550058";
-var PRODUCTION_DB = "mongodb+srv://chenop:selavi99@cluster-app27550058.nxme7.mongodb.net/heroku_app27550058?retryWrites=true&w=majority";
+var PRODUCTION_DB = "mongodb://chenop:selavi99@cluster-app27550058-shard-00-00.nxme7.mongodb.net:27017,cluster-app27550058-shard-00-01.nxme7.mongodb.net:27017,cluster-app27550058-shard-00-02.nxme7.mongodb.net:27017/heroku_app27550058?ssl=true&replicaSet=atlas-855ify-shard-0&authSource=admin&retryWrites=true&w=majority";
 
 var DEVELOPMENT_ADDRESS = 'http://localhost:3000';
 var DEVELOPMENT_DOC_PARSER = 'http://localhost:4200/webapi';
